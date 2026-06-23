41c6b315c42b6af98a663392a47eddde4fe6d234
Abdulfattox <abdulfattox@users.noreply.github.com>
2026-06-23 21:41:55 +0500

    Split production map search helpers

--- lib/src/features/admin/presentation/admin_production_map_orders_screen.dart
+++ lib/src/features/admin/presentation/admin_production_map_orders_screen.dart
@@ -60,6 +60,7 @@ part 'admin_production_map_orders_calculation_helpers.dart';
 part 'admin_production_map_orders_read_only_helpers.dart';
 part 'admin_production_map_orders_move_helpers.dart';
 part 'admin_production_map_orders_queue_helpers.dart';
+part 'admin_production_map_orders_search_helpers.dart';
 
 enum _OpenedOrderModule { orders, move, sequence, closed }
 
