4556060c7f8e613bf1a2cb6cc9d42e58593b7f2e
WIKKIwk <uzkingshark@gmail.com>
2026-07-10 18:38:43 +0500

    fix: use bunpod transition builder directly

--- lib/src/app/app_router.dart
+++ lib/src/app/app_router.dart
@@ -902,146 +902,39 @@ class AppRouter {
   }
 }
 
-class _BunpodPageRoute<T> extends PageRouteBuilder<T> {
+class _BunpodPageRoute<T> extends MaterialPageRoute<T> {
   _BunpodPageRoute({required RouteSettings settings, required Widget child})
-      : super(
-          settings: settings,
-          transitionDuration: const Duration(milliseconds: 450),
-          reverseTransitionDuration: const Duration(milliseconds: 450),
-          pageBuilder: (context, animation, secondaryAnimation) => child,
-          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-              _BunpodFadeForwardsPageTransition(
-            animation: animation,
-            secondaryAnimation: secondaryAnimation,
-            child: child,
-          ),
-        );
+      : super(settings: settings, builder: (context) => child);
+
+  static const FadeForwardsPageTransitionsBuilder _transitionBuilder =
+      FadeForwardsPageTransitionsBuilder();
 
   @override
-  DelegatedTransitionBuilder? get delegatedTransition => (
-        BuildContext context,
-        Animation<double> animation,
-        Animation<double> secondaryAnimation,
-        bool allowSnapshotting,
-        Widget? child,
-      ) =>
-          _buildBunpodDelegatedTransition(context, secondaryAnimation, child);
-}
-
-class _BunpodFadeForwardsPageTransition extends StatelessWidget {
-  const _BunpodFadeForwardsPageTransition({
-    required this.animation,
-    required this.secondaryAnimation,
-    required this.child,
-  });
-
-  final Animation<double> animation;
-  final Animation<double> secondaryAnimation;
-  final Widget child;
-
-  static final Animatable<Offset> _forwardTranslationTween = Tween<Offset>(
-    begin: const Offset(0.25, 0),
-    end: Offset.zero,
-  ).chain(CurveTween(curve: Curves.easeInOutCubicEmphasized));
-
-  static final Animatable<Offset> _backwardTranslationTween = Tween<Offset>(
-    begin: Offset.zero,
-    end: const Offset(0.25, 0),
-  ).chain(CurveTween(curve: Curves.easeInOutCubicEmphasized));
-
-  static final Animatable<double> _fadeInTransition = Tween<double>(
-    begin: 0,
-    end: 1,
-  ).chain(CurveTween(curve: const Interval(0, 0.75)));
-
-  static final Animatable<double> _fadeOutTransition = Tween<double>(
-    begin: 1,
-    end: 0,
-  ).chain(CurveTween(curve: const Interval(0, 0.25)));
+  Duration get transitionDuration => _transitionBuilder.transitionDuration;
 
   @override
-  Widget build(BuildContext context) {
-    return DualTransitionBuilder(
-      animation: animation,
-      forwardBuilder: (context, animation, child) {
-        return FadeTransition(
-          opacity: _fadeInTransition.animate(animation),
-          child: SlideTransition(
-            position: _forwardTranslationTween.animate(animation),
-            child: child,
-          ),
-        );
-      },
-      reverseBuilder: (context, animation, child) {
-        return IgnorePointer(
-          ignoring: animation.status == AnimationStatus.forward,
-          child: FadeTransition(
-            opacity: _fadeOutTransition.animate(animation),
-            child: SlideTransition(
-              position: _backwardTranslationTween.animate(animation),
-              child: child,
-            ),
-          ),
-        );
-      },
-      child:
-          _buildBunpodDelegatedTransition(context, secondaryAnimation, child),
+  Duration get reverseTransitionDuration =>
+      _transitionBuilder.transitionDuration;
+
+  @override
+  DelegatedTransitionBuilder? get delegatedTransition =>
+      _transitionBuilder.delegatedTransition;
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return _transitionBuilder.buildTransitions(
+      this,
+      context,
+      animation,
+      secondaryAnimation,
+      child,
     );
   }
-}
-
-Widget _buildBunpodDelegatedTransition(
-  BuildContext context,
-  Animation<double> secondaryAnimation,
-  Widget? child,
-) {
-  final builder = DualTransitionBuilder(
-    animation: ReverseAnimation(secondaryAnimation),
-    forwardBuilder: (context, animation, child) {
-      return FadeTransition(
-        opacity: _BunpodFadeForwardsPageTransition._fadeInTransition.animate(
-          animation,
-        ),
-        child: SlideTransition(
-          position: Tween<Offset>(
-            begin: const Offset(-0.25, 0),
-            end: Offset.zero,
-          ).chain(CurveTween(curve: Curves.easeInOutCubicEmphasized)).animate(
-                animation,
-              ),
-          child: child,
-        ),
-      );
-    },
-    reverseBuilder: (context, animation, child) {
-      return FadeTransition(
-        opacity: _BunpodFadeForwardsPageTransition._fadeOutTransition.animate(
-          animation,
-        ),
-        child: SlideTransition(
-          position: Tween<Offset>(
-            begin: Offset.zero,
-            end: const Offset(-0.25, 0),
-          ).chain(CurveTween(curve: Curves.easeInOutCubicEmphasized)).animate(
-                animation,
-              ),
-          child: child,
-        ),
-      );
-    },
-    child: child,
-  );
-
-  final bool isOpaque = ModalRoute.opaqueOf(context) ?? true;
-  if (!isOpaque) {
-    return builder;
-  }
-  return ColoredBox(
-    color: secondaryAnimation.isAnimating
-        ? ColorScheme.of(context).surface
-        : Colors.transparent,
-    child: builder,
-  );
 }
 
 class _CapabilityDeniedScreen extends StatelessWidget {
