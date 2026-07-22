23bef89769a0c03d4149766ab091fe379b667fde
WIKKIwk <uzkingshark@gmail.com>
2026-07-22 23:54:25 +0500

    Test incremental Bluetooth printer UI

---
+++ test/bluetooth_printer_list_test.dart
@@ -0,0 +1,64 @@
+import 'package:accord_mobile_v2/src/core/widgets/printing/bluetooth_printer_list.dart';
+import 'package:accord_mobile_v2/src/core/native_bluetooth_printer.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const discoveryChannel = EventChannel('accord/bluetooth_printer/discovery');
+
+  tearDown(() {
+    debugDefaultTargetPlatformOverride = null;
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockStreamHandler(discoveryChannel, null);
+  });
+
+  testWidgets('shows iOS printers before discovery completes', (tester) async {
+    final previousPlatform = debugDefaultTargetPlatformOverride;
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+    MockStreamHandlerEventSink? events;
+
+    try {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockStreamHandler(
+        discoveryChannel,
+        MockStreamHandler.inline(
+          onListen: (_, sink) {
+            events = sink;
+          },
+        ),
+      );
+
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: BluetoothPrinterList(onSelected: _ignoreSelection),
+          ),
+        ),
+      );
+      await tester.pump();
+
+      expect(find.byType(LinearProgressIndicator), findsOneWidget);
+      events!.success(<String, Object?>{
+        'type': 'printer',
+        'name': 'XP-P323B',
+        'address': 'ios-printer-1',
+      });
+      await tester.pump();
+
+      expect(find.text('XP-P323B'), findsOneWidget);
+      expect(find.text('ios-printer-1'), findsOneWidget);
+
+      events!.success(<String, Object?>{'type': 'complete'});
+      await tester.pump();
+      expect(find.text('XP-P323B'), findsOneWidget);
+    } finally {
+      debugDefaultTargetPlatformOverride = previousPlatform;
+    }
+  });
+}
+
+void _ignoreSelection(BluetoothPrinterProfile printer) {}
